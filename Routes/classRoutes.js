ddbe0d4e98b60ee91a1f893cfea3fa7ff9cce9f6
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-09-09 22:11:09 +0530

    merge

--- Routes/classRoutes.js
+++ Routes/classRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { AddClass, DeleteClass, GetOwnClasses, addSubjects, addSubTeacherPair, deleteSubTeacherPair } = require("../Controller/classController");
+const { AddClass, DeleteClass,  addSubjects, addSubTeacherPair, deleteSubTeacherPair } = require("../Controller/classController");
 const router = express.Router();
 
 
 router.post("/createNewClass", AddClass);
 router.delete("/deleteClass/:id", DeleteClass);
-router.get("/ownClasses", GetOwnClasses);
+// router.get("/ownClasses", GetOwnClasses);
 router.post("/addSubjects/:classId", addSubjects);
 router.post("/addSubTeacherPair/:classId", addSubTeacherPair);
 router.delete("/deleteSubTeacherPair/:classId", deleteSubTeacherPair);
